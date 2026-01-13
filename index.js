2703c3ce24773ffb1f1105ca0633a23bc1384767
AAVISTACK <63523107+AAVISTACK@users.noreply.github.com>
2026-01-13 08:41:04 +0000

    add whatsapp webhook

--- index.js
+++ index.js
@@ -15,4 +15,24 @@ app.get("/", (req, res) => {
 
     app.listen(PORT, "0.0.0.0", () => {
       console.log("Server running on port", PORT);
-      });
+      });// Meta webhook verification
+      app.get("/webhook", (req, res) => {
+        const VERIFY_TOKEN = "ankush123";
+
+          const mode = req.query["hub.mode"];
+            const token = req.query["hub.verify_token"];
+              const challenge = req.query["hub.challenge"];
+
+                if (mode === "subscribe" && token === VERIFY_TOKEN) {
+                    console.log("Webhook verified");
+                        return res.status(200).send(challenge);
+                          }
+                            return res.sendStatus(403);
+                            });
+
+                            // Receive messages
+                            app.post("/webhook", (req, res) => {
+                              console.log("Incoming message 👇");
+                                console.log(JSON.stringify(req.body, null, 2));
+                                  res.sendStatus(200);
+                                  });
